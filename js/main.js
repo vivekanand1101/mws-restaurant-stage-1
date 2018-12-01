900b4216a2d9e4e9f735356a9fa409eb8a85bc90
Vivek Anand <vivekanand1101@gmail.com>
2018-12-01 12:57:42 +0530

    Basic service worker registered
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- js/main.js
+++ js/main.js
@@ -11,8 +11,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
   initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
+  registerServiceWorkers();
 });
 
+registerServiceWorkers = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
 /**
  * Fetch all neighborhoods and set their HTML.
  */

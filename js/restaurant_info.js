d6dfc91926028c85ad12b7adc542f2ef930ecc30
Vivek Anand <vivekanand1101@gmail.com>
2018-12-02 20:12:27 +0530

    add more accessibility features
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = `image of ${restaurant.name}`
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
